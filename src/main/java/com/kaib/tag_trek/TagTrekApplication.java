2a50fd63a244025de05d8ebaedb6770d80dd9452
Jim Kaib <jkaibjr@gmail>
2025-07-03 09:25:23 -0400

    chore: migrate Spring API skeleton

---
+++ src/main/java/com/kaib/tag_trek/TagTrekApplication.java
@@ -0,0 +1,13 @@
+package com.kaib.tag_trek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagTrekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagTrekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaib/tag_trek/TagTrekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaib.tag_trek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagTrekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
